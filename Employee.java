cb9d996cba6d4f56add1905add0953a54e06f952
MAXXXLARD <maxlord@hotmail.ca>
2017-03-15 15:06:12 -0600

    Minor class development

--- Employee.java
+++ Employee.java
@@ -1,3 +1,31 @@
+
 public class Employee
 {
+    protected String empName;
+    protected String empNum;
+    protected String department;
+    protected char type;
+    
+    public Employee()
+    {
+        
+    }
+    
+    public Employee (String name, String eNum, String dep, char t)
+    {
+        empName = name;
+        eNum = empNum;
+        dep = department;
+        t = type;
+    }
+    
+    public String getName()
+    {
+        return empName;
+    }
+    
+    public void setName(String name)
+    {
+        empName = name;
+    }
 }
