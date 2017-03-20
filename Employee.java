06c66a2465c6955e02d82af7541e9f1192d9d6ae
CorvynH <corvynhartwick@Corvyns-MacBook-Pro.local>
2017-03-20 14:42:13 -0600

    angrey
    wow

--- Employee.java
+++ Employee.java
@@ -1,32 +1,18 @@
 
 public class Employee
 {
-<<<<<<< HEAD
-    String empName="";
-    String empNum="";
-    String depart="";
-    char type;
-    
-    public Employee ()
-=======
+
     protected String empName;
     protected String empNum;
     protected String department;
     protected char type;
-    
-    public Employee()
->>>>>>> Max-Branch
+
+    public Employee ()
     {
-        
+
     }
-    
-<<<<<<< HEAD
-    public Employee (String empName, String empNum, String depart, char type)
-    {
-        
-    }
-}
-=======
+
+
     public Employee (String name, String eNum, String dep, char t)
     {
         empName = name;
@@ -34,15 +20,15 @@ public class Employee
         dep = department;
         t = type;
     }
-    
+
     public String getName()
     {
         return empName;
     }
-    
+
     public void setName(String name)
     {
         empName = name;
     }
 }
->>>>>>> Max-Branch
+
